971a8801354ce45f1bed20b743640f88c8af4cf1
Jenkins <cajenkins@smu.edu>
2019-02-14 16:10:12 -0600

    game created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import characters from "./characters.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.characters to the characters json array
+  state = {
+    characters,
+    score: 0,
+    highscore: 0
+  };
+
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.characters.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+  clickCount = id => {
+  // eslint-disable-next-line
+    this.state.characters.find((o, i) => {
+      if (o.id === id) {
+        if(characters[i].count === 0){
+          characters[i].count = characters[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.characters.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Let's Test Your Memory With The Clicky Game</Header>
+        {this.state.characters.map(character => (
+          <Card
+            clickCount={this.clickCount}
+            id={character.id}
+            key={character.id}
+            image={character.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <div className="header">
+    <div className="title">{props.children}</div>
+    <div className="scores">
+      Score: {props.score} 
+      <br></br> Highscore: {props.highscore}
+    </div>
+  </div>
+);
+
+export default Header;
