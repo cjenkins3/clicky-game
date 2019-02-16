07f01e2fdec2fc22a944c150c1cbac19ed7f80b2
Jenkins <cajenkins@smu.edu>
2019-02-16 14:02:32 -0600

    Updates

---
+++ precache-manifest.fd3b3ebf6c267dea54bee437f89fa9e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "a005bb4a16cef708b711",
+    "url": "/clicky-game/static/js/main.a005bb4a.chunk.js"
+  },
+  {
+    "revision": "f39624b01d29e1627497",
+    "url": "/clicky-game/static/js/2.f39624b0.chunk.js"
+  },
+  {
+    "revision": "a005bb4a16cef708b711",
+    "url": "/clicky-game/static/css/main.4a471105.chunk.css"
+  },
+  {
+    "revision": "fceef6d97f0da75366dffbe168a029f5",
+    "url": "/clicky-game/index.html"
+  }
+];
